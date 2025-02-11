3771eb7fafc1aa7a42c541b4fc66a17cad8c537b
johanromeo <johan_romeo123@live.se>
2025-02-11 21:47:12 +0100

    added tech stack icons

--- src/components/Main/About/TechStack/TechStack.jsx
+++ src/components/Main/About/TechStack/TechStack.jsx
@@ -3,12 +3,51 @@ import "./TechStack.css";
 const TechStack = () => {
   return (
     <section className="tech-stack-section">
-      <h2>Technologies I use</h2>
+      <h2>Tech stack</h2>
       <div className="tech-items">
-        <h3>Backend</h3>
         <img
           className="tech-img"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg"
+        />
+
+        <img
+          className="tech-img"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original-wordmark.svg"
+        />
+
+        <img
+          className="tech-img"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg"
+        />
+
+        <img
+          className="tech-img"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg"
+        />
+
+        <img
+          className="tech-img"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg"
+        />
+
+        <img
+          className="tech-img"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+        />
+
+        <img
+          className="tech-img"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg"
+        />
+
+        <img
+          className="tech-img"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original-wordmark.svg"
+        />
+
+        <img
+          className="tech-img"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/raspberrypi/raspberrypi-original-wordmark.svg"
         />
       </div>
     </section>
