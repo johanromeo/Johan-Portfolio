54d8cbb2669e4c09ad6dd623beca9f172d019148
johanromeo <johan_romeo123@live.se>
2025-02-13 14:06:48 +0100

    simple hamburger

--- src/components/Header/Navigation/NavigationMenu.jsx
+++ src/components/Header/Navigation/NavigationMenu.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import ScrollSpy from "react-scrollspy-navigation";
 import "./NavigationMenu.css";
 
 const NavigationMenu = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <ScrollSpy activeClass="nav-active" offsetTop={80}>
-      <nav className="nav-container">
-        <a href="#target-0">Home</a>
-        <a href="#target-1">About</a>
-        <a href="#target-2">Projects</a>
-        <a href="#target-3">Contact</a>
-      </nav>
-    </ScrollSpy>
+    <div>
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
+      </button>
+      <ScrollSpy activeClass="nav-active" offsetTop={80}>
+        <nav className={`nav-container ${menuOpen ? "open" : ""}`}>
+          <a href="#target-0">Home</a>
+          <a href="#target-1">About</a>
+          <a href="#target-2">Projects</a>
+          <a href="#target-3">Contact</a>
+        </nav>
+      </ScrollSpy>
+    </div>
   );
 };
 
